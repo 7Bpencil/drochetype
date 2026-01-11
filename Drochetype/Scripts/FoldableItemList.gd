2f77696f614d9a66765ca5393d0d9f47a9bd081a
7Bpencil <Edward.Ekb@yandex.ru>
2026-01-12 01:00:43 +0500

    delete Godot implementation specific files

--- Drochetype/Scripts/FoldableItemList.gd
+++
@@ -1,5 +0,0 @@
-class_name FoldableItemList extends Node
-
-
-@export var foldable: FoldableContainer
-@export var list: ItemList
--- Drochetype/Scripts/TypingLayout.gd
+++
@@ -1,5 +0,0 @@
-class_name TypingLayout
-
-
-var lines: Array[Array]
-var letters_count: int
