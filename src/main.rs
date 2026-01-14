b28a4fcbea4e8b2fdbc65fee4c44ec881399b826
7Bpencil <Edward.Ekb@yandex.ru>
2026-01-14 20:05:59 +0500

    show completed test result, start new test, reset current test

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,15 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifier
 use ratatui::{
     buffer::Buffer,
     layout::{Rect, Constraint, Position, Size, Offset},
-    style::{Stylize, Color, Style},
+    style::{Stylize, Color, Style, Modifier},
+    symbols,
     symbols::border,
     text::{Span, Line, Text},
-    widgets::{Block, Paragraph, Widget, Borders},
+    widgets::{Block, Paragraph, Widget, Borders, Tabs, Clear},
     DefaultTerminal, Frame,
 };
 
-const MAX_LINES: usize = 6;
+const MAX_LINES: usize = 3;
 const MAX_LINE_LENGTH: usize = 45;
 const MAX_TOTAL_LENGTH: usize = MAX_LINES * MAX_LINE_LENGTH;
 
@@ -95,6 +96,9 @@ struct State {
     test_end_time: Instant,
     total_keys_pressed: usize,
     wrong_keys_pressed: usize,
+    show_settings: bool,
+    settings_tabs: Vec<SettingsTab>,
+    active_settings_tab_index: usize,
     exit: bool,
 }
 
@@ -112,6 +116,15 @@ struct TestData {
     char_positions: Vec<Position>,
 }
 
+enum SettingsTab {
+    Language,
+    Type,
+    WordsRarity,
+    IncludeLetter,
+    SelectLetters,
+    Size,
+}
+
 fn main() {
     setup_logging();
 
@@ -126,6 +139,9 @@ fn main() {
         test_end_time: time,
         total_keys_pressed: 0,
         wrong_keys_pressed: 0,
+        show_settings: false,
+        settings_tabs: vec![SettingsTab::Language, SettingsTab::Type, SettingsTab::WordsRarity, SettingsTab::IncludeLetter, SettingsTab::Size],
+        active_settings_tab_index: 0,
         exit: false,
     };
 
@@ -255,10 +271,40 @@ fn update(state: &mut State) -> Result<()> {
 }
 
 fn key_input(key_event: KeyEvent, state: &mut State) {
+    info!("input code: {}, modifiers: {}", key_event.code, key_event.modifiers);
+
     if key_event.modifiers.contains(KeyModifiers::CONTROL) && key_event.code.is_char('c') {
         state.exit = true;
         return;
     };
+
+    match key_event.code {
+        KeyCode::Esc => {
+            if state.show_settings {
+                state.show_settings = false;
+                return;
+            }
+        }
+        KeyCode::Enter => {
+            state.show_settings = !state.show_settings;
+            return;
+        },
+        KeyCode::Tab => {
+            state.show_settings = true;
+            state.active_settings_tab_index = (state.active_settings_tab_index + 1) % state.settings_tabs.len();
+            return;
+        },
+        KeyCode::BackTab => {
+            state.show_settings = true;
+            state.active_settings_tab_index = (state.settings_tabs.len() + state.active_settings_tab_index - 1) % state.settings_tabs.len();
+            return;
+        },
+        _ => {}
+    }
+
+    if state.show_settings {
+        return;
+    }
 
     match state.test_state {
         TestState::Waiting => match key_event.code {
@@ -327,6 +373,9 @@ fn reset_current_test(state: &mut State) {
 }
 
 fn render(frame: &mut Frame, state: &State) {
+    let frame_area = frame.area();
+
+    // draw background border
     let title_line = Line::from("drochetype");
     let test_state_line = match state.test_state {
         TestState::Waiting => Line::from("waiting"),
@@ -338,16 +387,50 @@ fn render(frame: &mut Frame, state: &State) {
         .title_bottom(test_state_line)
         .borders(Borders::ALL);
 
-    frame.render_widget(block, frame.area());
+    frame.render_widget(block, frame_area);
 
-    // result line with accuracy/wpm/cpm/time appears only on finished state,
-    // result line is placed below text,
-    // space for result line is reserved even when test is not finished,
-    // otherwise main text could jump one line up or down
-    // during running->finished or finished->waiting transitions
+    // draw main text
+    let text = generate_text_from_test(&state.test_data);
+    let text_area = Rect::new(
+        get_center(frame_area.width, MAX_LINE_LENGTH as u16),
+        get_center(frame_area.height, text.height() as u16),
+        MAX_LINE_LENGTH as u16,
+        text.height() as u16
+    );
+    frame.render_widget(text, text_area);
 
-    let test_data = &state.test_data;
-    let result_line = if state.test_state == TestState::Finished {
+    // draw cursor
+    if !state.show_settings {
+        let cursor_position_index = state.test_data.input_chars.len();
+        let cursor_position_local = state.test_data.char_positions[cursor_position_index];
+        frame.set_cursor_position(cursor_position_local + text_area.as_position().into());
+    }
+
+    // draw settings tabs
+    let separator = " • ";
+    let active_settings_tab_style = Style::default().add_modifier(Modifier::UNDERLINED);
+    let mut tab_names = Vec::with_capacity(state.settings_tabs.len() * 2 - 1);
+    for (i, settings_tab) in state.settings_tabs.iter().enumerate() {
+        let settings_tab_name = get_settings_tab_name(settings_tab);
+        if i == state.active_settings_tab_index {
+            tab_names.push(Span::styled(settings_tab_name, active_settings_tab_style));
+        } else {
+            tab_names.push(Span::from(settings_tab_name));
+        }
+        if i < state.settings_tabs.len() - 1 {
+            tab_names.push(Span::from(separator));
+        }
+    }
+
+    let active_tab_name_x_local = get_tab_name_x(state.active_settings_tab_index, &tab_names);
+    let tabs = Line::from(tab_names);
+    let tabs_width = tabs.width() as u16;
+    let tabs_area = Rect::new(get_center(frame_area.width, tabs_width), text_area.y - 2, tabs_width, 1);
+    let active_tab_name_x = active_tab_name_x_local + tabs_area.x - 2;
+    frame.render_widget(tabs, tabs_area);
+
+    // draw result line
+    if state.test_state == TestState::Finished {
         let elapsed_time = state.test_end_time.duration_since(state.test_start_time);
         let elapsed_seconds = elapsed_time.as_secs();
         let time_string = format!("{0:02}:{1:02}", elapsed_seconds / 60, elapsed_seconds % 60);
@@ -357,7 +440,7 @@ fn render(frame: &mut Frame, state: &State) {
         let cpm_string = cpm.floor().to_string();
         let wpm = cpm / 5.0;
         let wpm_string = wpm.floor().to_string();
-        Line::from(vec![
+        let result_line = Line::from(vec![
             Span::from("acc: "),
             Span::from(accuracy_string).fg(Color::Yellow),
             Span::from(" wpm: "),
@@ -366,27 +449,109 @@ fn render(frame: &mut Frame, state: &State) {
             Span::from(cpm_string).fg(Color::Yellow),
             Span::from(" time: "),
             Span::from(time_string).fg(Color::Yellow),
-        ])
-    } else {
-        Line::from("")
+        ]);
+        let result_line_area = Rect::new(text_area.x, text_area.bottom() + 1, frame_area.width, 1);
+        frame.render_widget(result_line, result_line_area);
     };
-    let text = generate_text_from_test(test_data, result_line);
 
-    let text_area = frame.area().centered(
-        Constraint::Length(MAX_LINE_LENGTH as u16),
-        Constraint::Length(text.height() as u16)
-    );
-
-    frame.render_widget(text, text_area);
-
-    let cursor_position_index = test_data.input_chars.len();
-    let cursor_position_local = test_data.char_positions[cursor_position_index];
-    frame.set_cursor_position(cursor_position_local + text_area.as_position().into());
+    // draw settings options
+    if state.show_settings {
+        let current_setting_tab = &state.settings_tabs[state.active_settings_tab_index];
+        let text = get_settings_options(current_setting_tab);
+        let title_name = get_settings_tab_name(current_setting_tab);
+        let title = Line::styled(title_name, active_settings_tab_style);
+        let text_area_width = title.width() + 4;
+        let text_area_height = text.height() + 2;
+        let text_area = Rect::new(active_tab_name_x, tabs_area.y, text_area_width as u16, text_area_height as u16);
+        let paragraph = Paragraph::new(text).block(Block::default().borders(Borders::ALL).title_top(title.centered()));
+        frame.render_widget(Clear, text_area);
+        frame.render_widget(paragraph, text_area);
+    }
 }
 
-fn generate_text_from_test<'a>(test_data: &TestData, result_line: Line<'a>) -> Text<'a> {
+fn get_center(parent_size: u16, child_size: u16) -> u16 {
+    ((parent_size - child_size) as f64 / 2.0).floor() as u16
+}
+
+fn get_tab_name_x(tab_index: usize, tab_names: &Vec<Span>) -> u16 {
+    let mut x: usize = 0;
+    for (i, tab_name) in tab_names.iter().enumerate() {
+        if i / 2 == tab_index {
+            break;
+        }
+        x += tab_name.width();
+    }
+    x as u16
+}
+
+fn get_settings_tab_name(tab: &SettingsTab) -> String {
+    let name = match tab {
+        SettingsTab::Language => "language",
+        SettingsTab::Type => "test type",
+        SettingsTab::WordsRarity => "words rarity",
+        SettingsTab::IncludeLetter => "include letter",
+        SettingsTab::SelectLetters => "select letters",
+        SettingsTab::Size => "test size",
+    };
+    name.to_string()
+}
+
+fn get_settings_options(settings_tab: &SettingsTab) -> Text {
+    match settings_tab {
+        SettingsTab::Language => {
+            Text::from(vec![
+                Line::from("numbers").centered(),
+                Line::from("symbols").centered(),
+                Line::from("english").centered().style(Style::default().fg(Color::Yellow)),
+                Line::from("russian").centered(),
+            ])
+        },
+        SettingsTab::Type => {
+            Text::from(vec![
+                Line::from("letters").centered(),
+                Line::from("bigrams").centered(),
+                Line::from("trigrams").centered(),
+                Line::from("words").centered().style(Style::default().fg(Color::Yellow)),
+            ])
+        },
+        SettingsTab::WordsRarity => {
+            Text::from(vec![
+                Line::from("very common").centered(),
+                Line::from("common").centered().style(Style::default().fg(Color::Yellow)),
+                Line::from("rare").centered(),
+                Line::from("very rare").centered(),
+            ])
+        },
+        SettingsTab::IncludeLetter => {
+            Text::from(vec![
+                Line::from("* a b c d e f").centered(),
+                Line::from("g h i j k l m").centered(),
+                Line::from("n o p q r s t").centered(),
+                Line::from("u v w x y z  ").centered(),
+            ])
+        },
+        SettingsTab::SelectLetters => {
+            Text::from(vec![
+                Line::from("letters").centered(),
+                Line::from("bigrams").centered(),
+                Line::from("trigrams").centered(),
+                Line::from("words").centered(),
+            ])
+        },
+        SettingsTab::Size => {
+            Text::from(vec![
+                Line::from("very small").centered(),
+                Line::from("small").centered().style(Style::default().fg(Color::Yellow)),
+                Line::from("medium").centered(),
+                Line::from("large").centered(),
+            ])
+        },
+    }
+}
+
+fn generate_text_from_test<'a>(test_data: &TestData) -> Text<'a> {
     let test_lines = &test_data.lines;
-    let mut lines = Vec::with_capacity(test_lines.len() + 1);
+    let mut lines = Vec::with_capacity(test_lines.len());
 
     let mut span_index = 0;
     for test_line in test_lines {
@@ -406,7 +571,6 @@ fn generate_text_from_test<'a>(test_data: &TestData, result_line: Line<'a>) -> T
         let line = Line::from(line_spans);
         lines.push(line);
     }
-    lines.push(result_line);
 
     Text::from(lines)
 }
